de191b5d5dbc0c59eb6c9f61ea93c52f57f32d22
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-17 10:36:47 +0200

    Example.lua

--- Example.lua
+++ Example.lua
@@ -1,1 +1,7 @@
+_G.Spawn
+return
+ Spawn function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HubStudioInjection/Protegen.Genic-Remastered/refs/heads/main/Obf-Source.lua", true))("ESP?")
+end)
+return
+ 
