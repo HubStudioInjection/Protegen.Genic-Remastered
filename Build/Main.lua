42edb28e413984dcdc1ee1915888c8a72b63531f
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-18 21:45:03 +0200

    Main.lua

--- Build/Main.lua
+++ Build/Main.lua
@@ -1,7 +1,10 @@
-local Tracers = Tracers Api.lua
+local Tracers = Tracers Api.lua;
+local ESP = ESP Api.lua;
 local Service = {
-  local Once = ESP or Tracers Once = false
+  local Once = ESP or Tracers Once = false;
   local Modules = {
-    local ToggleRuns = ESP and Tracers
+    local ToggleRuns = ESP and Tracers;
   }
-}
+} wait(1) while game:GetService("RunService").RenderStepped:wait()
+return tonumber;
+end
