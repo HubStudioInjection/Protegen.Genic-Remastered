cc282ffb30d116c7bfd56babc325cf00bbec42e1
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-19 09:54:29 +0200

    Example.lua

--- Example.lua
+++ Example.lua
@@ -1,8 +1,1 @@
-local ProtegenGenic = true
-return
- if ProtegenGenic == true then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HubStudioInjection/Protegen.Genic-Remastered/refs/heads/main/Obf-Source.lua", true))("ESP?")
-else
- warn("Protegen Genic Loaded")
-end
-return
