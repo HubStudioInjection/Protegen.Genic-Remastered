1e3e9423bace1c646f6bf6324e52ace2cd046a33
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-22 09:55:36 +0200

    Base V2.lua

--- Themes Build/Base V2.lua
+++ Themes Build/Base V2.lua
@@ -1,5 +1,6 @@
-local Base = 
-JSON{
+local httpService = game:GetService("httpService"); 
+local Base =  
+httpService:JSONDecode{
   "FontColor":"ffffff",
   "MainColor":"151515",
   "AccentColor":"5dffb9",
