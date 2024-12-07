d63b0bb906d93a48fb20c03c77f066a2d65c7233
HubStudioInjection <hubstudioijection@gmail.com>
2024-12-07 10:36:52 +0200

    Delete Themes Build directory

--- Themes Build/Base V2.lua
+++
@@ -1,9 +0,0 @@
-local httpService = game:GetService("httpService"); 
-local Base =  
-httpService:JSONDecode{
-  "FontColor":"ffffff",
-  "MainColor":"1c1c1c",
-  "AccentColor":"4b6bb2",
-  "BackgroundColor":"141414",
-  "OutlineColor":"424242"
-}
