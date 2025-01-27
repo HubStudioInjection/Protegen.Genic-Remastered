0b03ae4fc488b4337917e859453cc370610686d3
HubStudioInjection <hubstudioijection@gmail.com>
2025-01-27 11:00:46 +0200

    Example.lua

--- Example.lua
+++ Example.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HubStudioInjection/Protegen.Genic-Remastered/refs/heads/main/Source.lua", true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/HubStudioInjection/Protegen.Genic-Remastered/refs/heads/main/Source.lua"), true))()
